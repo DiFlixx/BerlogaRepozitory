7b7cfce996f0da4f2c2edc956577f0edc1116c06
DiFlixx <maksimspirin72@gmail.com>
2023-11-24 20:36:49 +0500

    TimeSystem

---
+++ Berloga/Assets/Scripts/DayTime.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DayTime
+{
+    Morning,
+    Day,
+    Evening,
+    Night,
+    Size
+}
---
+++ Berloga/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimeManager : MonoBehaviour
+{
+    public UnityEvent<DayTime> DayTimeChanged;
+
+    [SerializeField]
+    private float _fullDayLen = 720f;
+    [SerializeField]
+    private DayTime _startTime = DayTime.Day;
+
+    private float time;
+    private float _dayPartTime;
+    private int _currentDayTime;
+
+    private void Awake()
+    {
+        _dayPartTime = _fullDayLen / (int)DayTime.Size;
+        _currentDayTime = (int)_startTime;
+    }
+
+    public DayTime GetDayTime()
+    {
+        return (DayTime)_currentDayTime; 
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > _dayPartTime) 
+        {
+            time = 0;
+            _currentDayTime = (_currentDayTime + 1)%(int)DayTime.Size;
+            DayTimeChanged?.Invoke(GetDayTime());
+        }
+        
+    }
+}
