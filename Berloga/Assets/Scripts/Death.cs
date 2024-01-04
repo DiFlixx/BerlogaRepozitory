d790e262e000e96861978e26cc344faf53efbd4f
RoilesGame <roilesgame0799@gmail.com>
2024-01-05 01:41:35 +0300

    Сделал смерть

---
+++ Berloga/Assets/Scripts/Death.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("DeathScene");
+        }
+    }
+}
