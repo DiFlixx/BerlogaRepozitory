105492335fa916d3a8244791bb4175439c6eb68d
RoilesGame <roilesgame0799@gmail.com>
2024-01-05 21:25:37 +0300

    хз что писать

--- Berloga/Assets/Scripts/MainMenuScripts/ExitGameButton.cs
+++ Berloga/Assets/Scripts/MainMenuScripts/ExitGameButton.cs
@@ -1,10 +1,11 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ExitGameButton : MonoBehaviour
 {
     private Button _quitButton;
-    
+
     void Start()
     {
         _quitButton = GetComponent<Button>();
