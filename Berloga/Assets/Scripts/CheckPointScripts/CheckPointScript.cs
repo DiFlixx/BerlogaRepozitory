2093d7b5ba731c6bff2acfbf6a4e114f52db51c4
RoilesGame <roilesgame0799@gmail.com>
2024-01-05 21:41:00 +0300

    начальник, принимай

--- Berloga/Assets/Scripts/CheckPointScripts/CheckPointScript.cs
+++ Berloga/Assets/Scripts/CheckPointScripts/CheckPointScript.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CheckPointScript : MonoBehaviour
 {
-    public Transform player;
+    public Transform player1;
+    public Transform player2;
+    
     public int index;
 
     void Awake()
     {
-        player = GameObject.Find("MainCharacter").transform;
+        player1 = GameObject.Find("MainCharacter").transform;
+        player2 = GameObject.Find("MainCharacter2").transform;
 
         if (DataContainer.checkpointindex == index) 
         {
-            player.position = transform.position;
+            player1.position = transform.position;
+            player2.position = player1.position;
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "MainCharacter")
+        if (other.gameObject.name == "MainCharacter" | other.gameObject.name == "MainCharacter2")
         {
             DataContainer.checkpointindex = index;
         }
